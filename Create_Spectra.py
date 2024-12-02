e749e1d1f28dfdc530721ba2e0661155f5ec2ab7
lorenzopasquetto <109760305+lorenzopasquetto@users.noreply.github.com>
2024-12-02 17:25:19 +0100

    Create Create_Spectra.py

---
+++ Create_Spectra.py
@@ -0,0 +1,42 @@
+mport numpy as np
+
+from pymatgen.core import Lattice, Structure
+from pymatgen.analysis.diffraction.xrd import XRDCalculator
+import matplotlib.pyplot as plt
+from scipy.signal import convolve
+
+
+def lorentzian_kernel(x, gamma = 0.01):
+    return gamma / (np.pi * (x**2 + gamma**2))
+
+a = np.arange(-2, 2, 0.01)
+lorentzian = lorentzian_kernel(a)
+lorentzian /= np.sum(lorentzian_kernel(a))
+
+
+
+def create_dataset_cubic(lattice_parameter, species):
+    
+    a = lattice_parameter  
+    lattice = Lattice.cubic(a)
+
+    # Atomic species and their coordinates in the unit cell
+    species = [species, species]
+    coords = [[0, 0, 0], [0.5, 0.5, 0.5]]
+
+    # Create structure
+    structure = Structure(lattice, species, coords)
+
+    xrd_calculator = XRDCalculator(wavelength="CuKa")
+
+    pattern = xrd_calculator.get_pattern(structure)
+
+    peaks_ = np.round(pattern.x * 4500/90).astype(int)
+
+
+    init_array = np.zeros(4501)
+    init_array[peaks_] = pattern.y  # This is the spectra of Delta peaks with different intensities
+
+    spectra = convolve(init_array, lorentzian, mode='same')
+    spectra = (spectra * 1000) / (np.max(spectra))
+    
