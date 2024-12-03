b3214fc5552b79a11e5ba0affd011115b443e47a
lorenzopasquetto <109760305+lorenzopasquetto@users.noreply.github.com>
2024-12-03 17:21:42 +0100

    Update Create_Spectra.py

--- Create_Spectra.py
+++ Create_Spectra.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+import os
+from pymatgen.io.cif import CifParser
+
+
 from pymatgen.core import Lattice, Structure
 from pymatgen.analysis.diffraction.xrd import XRDCalculator
 import matplotlib.pyplot as plt
@@ -67,6 +71,72 @@ elements = [
     "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk",
     "Cf"
     ]
+
+
+cif_folder = "/Users/loernzopasquetto/Desktop/cod/cif/7/00/01"
+
+def Lattice_fp(x):
+    
+    
+    return  Lattice.from_parameters(x[0], x[1], x[2], x[3], x[4], x[5])
+
+def read_folder(cif_folder):
+    """
+    reads the folder and gives:
+    1- the coords of elements in the basis
+    2- the lattice parameters
+    3- the species
+    """
+    species = []
+    coord = []
+    lattice_params = []
+
+
+    for ind, i in enumerate(os.listdir(cif_folder)):
+        cif_path = os.path.join(cif_folder, i)
+        
+                
+        try:
+            parser = CifParser(cif_path)
+            structures = parser.get_structures()
+            if structures:
+                #print("Parsed structure:", structures[0])
+                for struc in structures:
+                    species.append(struc.species)
+                    coord.append(struc.frac_coords)
+                    lattice_params.append(struc.lattice.parameters)
+                    
+            else:
+                print("No structures found in the CIF file.")
+        except Exception as e:
+            print("Error parsing CIF file:", e)
+    
+    return species, coord, lattice_params
+
+
+"""
+Example:
+# Read all the .cif file in the folder and plot the spectra number 33
+
+
+cif_folder = "~/Desktop/cod/cif/7/00/01"
+X, Y, Z = read_folder(cif_folder)
+
+structure = Structure(Lattice_fp(Z[33]), X[33], Y[33])
+xrd_calculator = XRDCalculator(wavelength="CuKa")
+
+pattern = xrd_calculator.get_pattern(structure) 
+
+peaks_ = np.round(pattern.x * 4500/90).astype(int)
+init_array = np.zeros(4501)
+init_array[peaks_] = pattern.y  # This is the spectra of Delta peaks with different intensities
+
+spectra = convolve(init_array, lorentzian, mode='same')
+spectra = (spectra * 1000) / (np.max(spectra))
+final_spectra = spectra[:4500] # final shape is (4500, 1)
+
+"""
+
 
 
 #################################################
