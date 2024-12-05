06813d86aa6f76a39ebd117631ec78d2b4a9abd6
lorenzopasquetto <109760305+lorenzopasquetto@users.noreply.github.com>
2024-12-05 13:13:59 +0100

    Update Create_Spectra.py

--- Create_Spectra.py
+++ Create_Spectra.py
@@ -170,6 +170,32 @@ def create_data_set(species, coord, lattice_params):
     return np.stack(spectrum, axis = 0)
 
 
+def generate_coords_basis(n_basis):
+    """
+    Generate n_basis dimensional lists representing coords (coordinates of element in the basis) and
+    the different type of elements. The output structures are completilly artificial!
+    """
+
+    tri = [[0, 0, 0.25], [0, 0.25, 0.25], [0.25, 0.25, 0.25], [0, 0, 0.5], [0, 0.25, 0.5], [0, 0.5, 0.5], [0.25, 0.5, 0.5]]
+    elements = [
+    "H", "D", "T", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg",
+    "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn",
+    "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb",
+    "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In",
+    "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm",
+    "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta",
+    "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At",
+    "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk",
+    "Cf"
+    ]
+
+    coords = init_tri + random.sample(tri, n_basis-1)
+    elems = random.sample(elements, n_basis)
+
+    return coords, elems
+
+
+
 #################################################
 count = 0
 range_val=25
